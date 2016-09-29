16d87fb282186437477930a38d22ef260e2d3827
nishantdania <nishantdania@gmail.com>
2016-09-29 19:09:04 +0530

    added user model

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express'),
+	app = express(),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	config = require('./config/main');
+
+mongoose.connect(config.database);
+
+const server = app.listen(config.port, function () {
+  console.log('Example app listening on port ' + config.port + '!');
+});
+
+app.use(bodyParser.urlencoded({ extended: false  }));  
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+ });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
