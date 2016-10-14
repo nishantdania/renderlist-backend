b141cdda4c01ed6c5be041d091a5225bc0f95c83
nishantdania <nishantdania@gmail.com>
2016-10-14 11:28:17 +0530

    added fb login

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+	'facebook' : {
+        'clientID'      : '373030956419279', // your App ID
+        'clientSecret'  : '4daa64be0150a6bb3962e966b16457f0', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/api/auth/facebook/callback'
+	}
+};
