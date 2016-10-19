1231627564bb3d84a4b54d3d24636a72fce80e1c
nishantdania <nishantdania@gmail.com>
2016-10-19 12:53:32 +0530

    added getMyStudio

--- controllers/studio.js
+++ controllers/studio.js
@@ -1,6 +1,7 @@
 const Studio = require('../models/studio');
 	
 exports.addStudio = function (req, res) {
+console.log(req.user);
 	var studioData = req.body;
 	var studio = new Studio();
 	studio.name = studioData.name;
@@ -8,15 +9,23 @@ exports.addStudio = function (req, res) {
 	studio.city = studioData.city;
 	studio.description = studioData.description;
 	studio.showreelURL = studioData.showreelURL;
+	studio._user = req.user._id;
 	studio.save( function(err) {
 		if(err) res.send('error');
 	});
-	res.send('hmm');
+	res.status(200).send();
 }
 
 exports.getStudios = function (req, res) {
 	Studio.find({}, function (err, studios) {
 		if(err) res.send(err);
 		res.send(studios);
+	});	
+}
+
+exports.getMyStudio = function (req, res) {
+	Studio.find({'_user' : req.user.id}, function (err, studio) {
+		if(err) res.send(err);
+		res.send(studio);
 	});	
 }
