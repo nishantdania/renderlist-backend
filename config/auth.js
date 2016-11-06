11248f24ceeb1524745c07d7763fb3b4f3429614
nishantdania <nishantdania@gmail.com>
2016-11-06 14:19:20 +0530

    added env variables

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,30 @@
-module.exports = {
-
-	'facebook' : {
-        'clientID'      : '373030956419279', // your App ID
-        'clientSecret'  : '4daa64be0150a6bb3962e966b16457f0', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/api/auth/facebook/callback'
-	},
-	'google' : {
-        'clientID'      : '1070104330614-gdeqjtaj2eju0pr3ru6h5vgf7vccpbsk.apps.googleusercontent.com', // your App ID
-        'clientSecret'  : 'KbDcRF8O2br_0Uf9tqODKSvV', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/api/auth/google/callback'
+module.exports = new function () {
+	switch (process.env.NODE_ENV) {
+		case 'production' :
+			return {
+			'facebook' : {
+				'clientID'      : '373030956419279', // your App ID
+				'clientSecret'  : '4daa64be0150a6bb3962e966b16457f0', // your App Secret
+				'callbackURL'   : 'http://www.renderlist.com/api/auth/facebook/callback'
+			},
+			'google' : {
+				'clientID'      : '1070104330614-gdeqjtaj2eju0pr3ru6h5vgf7vccpbsk.apps.googleusercontent.com', // your App ID
+				'clientSecret'  : 'KbDcRF8O2br_0Uf9tqODKSvV', // your App Secret
+				'callbackURL'   : 'http://www.renderlist.com/api/auth/google/callback'
+			}	
+		};
+		default :
+			return {
+			'facebook' : {
+				'clientID'      : '373030956419279', // your App ID
+				'clientSecret'  : '4daa64be0150a6bb3962e966b16457f0', // your App Secret
+				'callbackURL'   : 'http://localhost:3000/api/auth/facebook/callback'
+			},
+			'google' : {
+				'clientID'      : '1070104330614-gdeqjtaj2eju0pr3ru6h5vgf7vccpbsk.apps.googleusercontent.com', // your App ID
+				'clientSecret'  : 'KbDcRF8O2br_0Uf9tqODKSvV', // your App Secret
+				'callbackURL'   : 'http://localhost:3000/api/auth/google/callback'
+			}	
+		};
 	}
-
 };
--- config/main.js
+++ config/main.js
@@ -1,6 +1,19 @@
-module.exports = {
-	'loginTokenExpiration': 2592000, // 1 month
-	'secret': 'carpediemdafaq',
-	'database': 'mongodb://nishantdania:conjur3r@ds017672.mlab.com:17672/users',
-	'port': process.env.PORT || 3000
+module.exports = new function () {
+	switch (process.env.NODE_ENV) {
+	case 'production' :
+		return {
+			'loginTokenExpiration': 2592000, // 1 month
+			'secret': 'carpediemdafaq',
+			'database': 'mongodb://localhost:27017/renderlist',
+			'port': process.env.PORT || 3000
+		};
+
+	default :
+		return {
+			'loginTokenExpiration': 2592000, // 1 month
+			'secret': 'carpediemdafaq',
+			'database': 'mongodb://nishantdania:conjur3r@ds017672.mlab.com:17672/users',
+			'port': process.env.PORT || 3000
+		};
+	}
 }
