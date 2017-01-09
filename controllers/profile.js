d943f47c07de6a869e04207fd4b6b22dd27b4fc8
nishantdania <nishantdania@gmail.com>
2017-01-09 17:38:34 +0530

    added edit tags

--- controllers/profile.js
+++ controllers/profile.js
@@ -29,6 +29,12 @@ exports.incLikes = function(req, res) {
 	}
 }
 
+exports.editTags = function(req, res) {
+	Studio.findByIdAndUpdate(req.body.sid, { 'tags' : req.body.tags }, function(err, studio) {
+		res.status(200).send({'success' : true});
+	});
+}
+
 exports.getMyProfile = function (req, res) {
 	Username.findOne({'username' : req.body.username}, function (err, username) {
 		if (err) res.status(400).send({'success' : false});
